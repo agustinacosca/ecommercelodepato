bb2ed805ae50b5040af8a9805ddfdbfc6586b940
Silvana Agustina Coscarrelli <silvanaagustinacoscarelli@gmail.com>
2024-11-13 14:03:25 -0300

    modificacion js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
 
 // main.js
-fetch('/header.html')
+fetch('#header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header').innerHTML = data;
   })
   .catch(error => console.log('Error al cargar el header:', error));
 
-fetch('./footer.html')
+fetch('footer.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('footer').innerHTML = data;
